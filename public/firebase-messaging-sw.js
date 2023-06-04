692cb8b91946e36ae18278f8d501b6eab1f9f760
qaseem <qaseemelahi@gmail.com>
2023-06-04 18:23:27 +0500

    update messaging-sw file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,6 @@
 // Scripts for firebase and firebase messaging
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js");
 
 const firebaseConfig = {
   apiKey: "AIzaSyDxQrKcFq1vNxuqPKw1YIG81p4WMdWGP5g",
